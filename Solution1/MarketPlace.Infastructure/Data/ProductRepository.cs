ca415952ca2a80ff10919c1d2d174528ebaa529a
Albert <user@WINDOWS-O7N9M38>
2023-01-26 18:28:31 +0400

    implement repository interfaces

---
+++ Solution1/MarketPlace.Infastructure/Data/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using MarketPlace.Core.Entities;
+using MarketPlace.Core.Interfaces;
+
+namespace MarketPlace.Infastructure.Data;
+public class ProductRepository : BaseRepository<Product>, IProductRepository
+{
+    private readonly AppDbContext _context;
+
+    public ProductRepository(AppDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task AddProductAsync(Product product)
+    {
+        await AddAsync(product);
+    }
+
+    public void UpdateProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+}
