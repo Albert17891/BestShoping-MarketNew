ed97487a6a850a09f90eec228e074d0dde8ee32a
Albert <user@WINDOWS-O7N9M38>
2023-01-26 18:18:43 +0400

    add AppDbContext

---
+++ Solution1/MarketPlace.Infastructure/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using MarketPlace.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketPlace.Infastructure.Data;
+
+public class AppDbContext : IdentityDbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<UserProduct> UserProducts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<UserProduct>()
+            .HasKey(x => new { x.UserId, x.ProductId });
+
+        builder.Entity<UserProduct>()
+            .HasOne<Product>(x => x.Product)
+            .WithMany(x => x.UsersProducts)
+            .HasForeignKey(x => x.ProductId);
+
+        builder.Entity<UserProduct>()
+            .HasOne<AppUser>(x => x.AppUser)
+            .WithMany(x => x.UserProducts)
+            .HasForeignKey(x => x.UserId);
+
+
+        base.OnModelCreating(builder);
+    }
+}
