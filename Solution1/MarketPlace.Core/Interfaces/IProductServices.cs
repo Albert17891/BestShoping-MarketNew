c681f708b1beeba6b66d6f15984196edc4c76339
Albert <user@WINDOWS-O7N9M38>
2023-01-26 18:27:58 +0400

    add IProductService and implement it

---
+++ Solution1/MarketPlace.Core/Interfaces/IProductServices.cs
@@ -0,0 +1,8 @@
+﻿using MarketPlace.Core.Entities;
+
+namespace MarketPlace.Core.Interfaces;
+public interface IProductService
+{
+    Task<IList<Product>> GetProductsAsync(CancellationToken cancellationToken);
+    Task<int> AddProductAsync(Product product, CancellationToken cancellationToken);
+}
---
+++ Solution1/MarketPlace.Core/Services/ProductServices.cs
@@ -0,0 +1,34 @@
+﻿using MarketPlace.Core.Entities;
+using MarketPlace.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketPlace.Core.Services;
+public class ProductServices : IProductService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ProductServices(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+
+    }
+    public async Task<int> AddProductAsync(Product product, CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();       
+
+        await _unitOfWork.ProductRepository.AddProductAsync(product);
+
+        await _unitOfWork.SaveChangeAsync();
+
+        return product.Id;
+    }
+
+    public async Task<IList<Product>> GetProductsAsync(CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        var products = await _unitOfWork.ProductRepository.Table.ToListAsync();
+
+        return products;
+    }
+}
