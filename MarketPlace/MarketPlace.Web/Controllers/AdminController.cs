847fcd80335cb0065227bf74b8a8dc1739165b48
Albert <user@WINDOWS-O7N9M38>
2023-01-28 17:36:15 +0400

    add AdminControllers

---
+++ MarketPlace/MarketPlace.Web/Controllers/AdminController.cs
@@ -0,0 +1,39 @@
+﻿using MarketPlace.Core.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketPlace.Web.Controllers;
+[Route("[controller]")]
+[ApiController]
+
+[Authorize(Roles ="Admin")]
+public class AdminController : ControllerBase
+{
+    private readonly UserManager<AppUser> _userManager;
+
+    public AdminController(UserManager<AppUser> userManager)
+	{
+		_userManager = userManager;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetUsers()
+	{
+		var users = _userManager.Users.ToListAsync();
+
+		if (users == null)
+			return NotFound();
+
+		return Ok(users);
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> UpdateRole()
+	{
+		//Todo
+		return NotFound();
+	}
+}
