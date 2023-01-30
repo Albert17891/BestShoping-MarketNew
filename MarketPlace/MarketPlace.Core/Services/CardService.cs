bf7d20ffa32dfb497cad860219efd589c359e387
Albert <user@WINDOWS-O7N9M38>
2023-01-30 13:44:48 +0400

    add card service

---
+++ MarketPlace/MarketPlace.Core/Services/CardService.cs
@@ -0,0 +1,32 @@
+﻿using MarketPlace.Core.Entities;
+using MarketPlace.Core.Interfaces.Repository;
+using MarketPlace.Core.Interfaces.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace MarketPlace.Core.Services;
+
+public class CardService : ICardService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CardService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task AddCardProductsAsync(UserProductCard userProduct, CancellationToken token)
+    {
+        token.ThrowIfCancellationRequested();
+
+        await _unitOfWork.CardRepository.AddAsync(userProduct);
+        await _unitOfWork.SaveChangeAsync();
+    }
+
+    public async Task<IList<UserProductCard>> GetCardProductsAsync(string userId, CancellationToken token)
+    {
+        token.ThrowIfCancellationRequested();
+
+        return await _unitOfWork.CardRepository.Table.Where(x => x.UserId == userId)
+                                                 .ToListAsync();
+    }
+}
