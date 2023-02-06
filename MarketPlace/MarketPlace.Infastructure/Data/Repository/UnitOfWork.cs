0e699235b66472ddac25e03e2f26021fa6217386
Albert <user@WINDOWS-O7N9M38>
2023-02-06 14:07:50 +0400

    implement IBaseReposiotry in unit of work

--- MarketPlace/MarketPlace.Infastructure/Data/Repository/UnitOfWork.cs
+++ MarketPlace/MarketPlace.Infastructure/Data/Repository/UnitOfWork.cs
@@ -6,15 +6,30 @@ namespace MarketPlace.Infastructure.Data.Repository;
 public class UnitOfWork : IUnitOfWork
 {
     private readonly AppDbContext _context;
+    private Dictionary<Type,object> _repositories;
 
     public UnitOfWork(AppDbContext context)
     {
-        _context = context;
-        ProductRepository = new ProductRepository(_context);
-        CardRepository = new CardRepository(_context);
+        _context = context;      
+    }  
+
+    public IBaseRepository<T> Repository<T>() where T : class
+    {
+        if(_repositories is null)
+        {
+            _repositories = new Dictionary<Type, object>();
+        }
+
+        var type = typeof(T);
+        if (!_repositories.ContainsKey(type))
+        {
+            var repositoryType = typeof(BaseRepository<>);
+            var repositoryInstance = Activator.CreateInstance(repositoryType.MakeGenericType(type), _context);
+            _repositories[type] = repositoryInstance;
+        }
+
+        return (IBaseRepository<T>)_repositories[type];
     }
-    public IProductRepository ProductRepository { get; }
-    public ICardRepository CardRepository { get; }
 
     public async Task<int> SaveChangeAsync()
     {
