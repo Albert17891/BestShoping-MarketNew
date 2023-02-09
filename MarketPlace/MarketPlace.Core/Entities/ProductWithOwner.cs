1839136bdb72a174ac81be176545b4328fdd65b8
Albert <user@WINDOWS-O7N9M38>
2023-02-09 16:55:12 +0400

    add new Models

---
+++ MarketPlace/MarketPlace.Core/Entities/ProductWithOwner.cs
@@ -0,0 +1,11 @@
+﻿namespace MarketPlace.Core.Entities;
+public class ProductWithOwner
+{
+    public string Name { get; set; }   
+    public int Quantity { get; set; }
+    public double Price { get; set; }
+
+    //Owner FirsName
+    public string FirstName { get; set; }
+    public string Email { get; set; }
+}
