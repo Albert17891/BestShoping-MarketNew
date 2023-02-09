5f300bade713ee08185d66b1f281f810f9f3befc
Albert <user@WINDOWS-O7N9M38>
2023-02-09 14:05:31 +0400

    add AdminService

---
+++ MarketPlace/MarketPlace.Core/Interfaces/Services/IAdminService.cs
@@ -0,0 +1,9 @@
+﻿using MarketPlace.Core.Entities;
+
+namespace MarketPlace.Core.Interfaces.Services;
+public interface IAdminService
+{
+    Task<bool> UpdateUserAsync(AppUser user, CancellationToken token);
+    Task<bool> DeleteUserAsync(AppUser user, CancellationToken token);
+
+}
---
+++ MarketPlace/MarketPlace.Core/Services/AdminService.cs
@@ -0,0 +1,48 @@
+﻿using MarketPlace.Core.Entities;
+using MarketPlace.Core.Exceptions;
+using MarketPlace.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Identity;
+
+namespace MarketPlace.Core.Services;
+
+public class AdminService : IAdminService
+{
+    private readonly UserManager<AppUser> _userManager;
+
+    public AdminService(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<bool> DeleteUserAsync(AppUser user, CancellationToken token)
+    {
+        token.ThrowIfCancellationRequested();
+
+        var existUser = await _userManager.FindByEmailAsync(user.Email);
+
+        if (existUser == null)
+            throw new UserNotFoundException("User Not Found ");
+
+        var result = await _userManager.DeleteAsync(existUser);
+
+        return result.Succeeded;
+    }
+
+    public async Task<bool> UpdateUserAsync(AppUser user, CancellationToken token)
+    {
+        token.ThrowIfCancellationRequested();
+
+        var existUser = await _userManager.FindByEmailAsync(user.Email);
+
+        if (user == null)
+            throw new UserNotFoundException("User Not Found ");
+
+        existUser.FirstName = user.FirstName;
+        existUser.LastName = user.LastName;
+        existUser.Email = user.Email;
+
+        var result = await _userManager.UpdateAsync(existUser);
+
+        return result.Succeeded;
+    }
+}
