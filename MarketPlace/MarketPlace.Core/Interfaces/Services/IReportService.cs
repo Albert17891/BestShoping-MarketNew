7b8790c7ce32f8cc1264895fafb11cbc4cb59a7e
Albert <user@WINDOWS-O7N9M38>
2023-02-10 19:49:22 +0400

    add new Report Service

---
+++ MarketPlace/MarketPlace.Core/Interfaces/Services/IReportService.cs
@@ -0,0 +1,8 @@
+﻿using MarketPlace.Core.Entities.Admin.Report;
+
+namespace MarketPlace.Core.Interfaces.Services;
+
+public interface IReportService
+{
+    Task<IList<TopProduct>> GetTopTenProductsAsync(CancellationToken cancellationToken);
+}
---
+++ MarketPlace/MarketPlace.Core/Services/ReportService.cs
@@ -0,0 +1,35 @@
+﻿using Mapster;
+using MarketPlace.Core.Entities;
+using MarketPlace.Core.Entities.Admin.Report;
+using MarketPlace.Core.Interfaces.Repository;
+using MarketPlace.Core.Interfaces.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MarketPlace.Core.Services;
+
+public class ReportService : IReportService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMediator _mediator;
+
+    public ReportService(IUnitOfWork unitOfWork,IMediator mediator)
+    {
+        _unitOfWork = unitOfWork;
+        _mediator = mediator;
+    }
+    public async Task<IList<TopProduct>> GetTopTenProductsAsync(CancellationToken cancellationToken)
+    {
+        var topProducts = await _unitOfWork.Repository<UserProductCard>().Table.GroupBy(x => x.ProductId)
+                                       .Select(x=>new TopProduct
+                                        {                                         
+                                         SumQuantity = x.Sum(x => x.Quantity),
+                                         Name=x.Select(x=>x.Name).First(),
+                                         Price=x.Select(x=>x.Price).First()
+                                       }).OrderByDescending(x=>x.SumQuantity)
+                                       .ToListAsync(cancellationToken);
+
+        return topProducts;
+    }
+}
