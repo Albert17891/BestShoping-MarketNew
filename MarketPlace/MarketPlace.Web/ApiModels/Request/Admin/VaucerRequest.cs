03974f1090f51dfdd7784bf0d183fcde24a23290
Albert <user@WINDOWS-O7N9M38>
2023-02-10 19:48:21 +0400

    add new model and mapping class

---
+++ MarketPlace/MarketPlace.Web/ApiModels/Request/Admin/VaucerRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MarketPlace.Web.ApiModels.Request.Admin;
+
+public class VaucerRequest
+{
+    public string UserId { get; set; }
+    public int ProductId { get; set; }
+    public DateTime ExpireTime { get; set; }
+    public double Price { get; set; }
+}
