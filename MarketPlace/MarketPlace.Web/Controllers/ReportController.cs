04307fa54140ea59cd3827d7b599643670c6a3ee
Albert <user@WINDOWS-O7N9M38>
2023-02-10 19:47:36 +0400

    add report controller

---
+++ MarketPlace/MarketPlace.Web/Controllers/ReportController.cs
@@ -0,0 +1,28 @@
+﻿using MarketPlace.Core.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketPlace.Web.Controllers;
+[Route("[controller]")]
+[ApiController]
+[Authorize(Roles ="Admin")]
+
+public class ReportController : ControllerBase
+{
+    private readonly IReportService _reportService;
+
+    public ReportController(IReportService reportService)
+	{
+		_reportService = reportService;
+	}
+
+	[Route("get-top-ten")]
+	[HttpGet]
+	public async Task<IActionResult> GetTopProducts(CancellationToken cancellationToken=default)
+	{
+		var result = await _reportService.GetTopTenProductsAsync(cancellationToken);
+
+		return Ok();
+	}
+}
