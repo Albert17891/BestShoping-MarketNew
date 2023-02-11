eda31d2f63a0f8bb82b70c43172cee5c10289ac2
Albert <user@WINDOWS-O7N9M38>
2023-02-11 19:26:25 +0400

    add new Api Models

---
+++ MarketPlace/MarketPlace.Web/ApiModels/Request/User/BuyProductInfoRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace MarketPlace.Web.ApiModels.Request.User;
+
+public class BuyProductInfoRequest
+{
+    public int ProductId { get; set; }
+    public double Price { get; set; }
+}
---
+++ MarketPlace/MarketPlace.Web/ApiModels/Response/UserAccountResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace MarketPlace.Web.ApiModels.Response;
+
+public class UserAccountResponse
+{
+    public double Amount { get; set; }
+}
