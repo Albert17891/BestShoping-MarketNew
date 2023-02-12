cc6bb542b1d19d30d1722e6f2eb861cd1b4568f7
Albert <user@WINDOWS-O7N9M38>
2023-02-12 18:54:12 +0400

    add new migrations

--- MarketPlace/MarketPlace.Infastructure/Migrations/20230212092043_Add_SumPrice_Property.Designer.cs
+++ MarketPlace/MarketPlace.Infastructure/Migrations/20230212092043_Add_SumPrice_Property.Designer.cs
@@ -4,6 +4,7 @@ using MarketPlace.Infastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MarketPlace.Infastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230212092043_Add_SumPrice_Property")]
+    partial class AddSumPriceProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -147,9 +150,6 @@ namespace MarketPlace.Infastructure.Migrations
                         .HasColumnType("int");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
-
-                    b.Property<DateTime>("BoughtTime")
-                        .HasColumnType("datetime2");
 
                     b.Property<bool>("IsActive")
                         .HasColumnType("bit");
---
+++ MarketPlace/MarketPlace.Infastructure/Migrations/20230212104500_Add_BouthTime_Property.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketPlace.Infastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBouthTimeProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BoughtTime",
+                table: "UserProductCard",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BoughtTime",
+                table: "UserProductCard");
+        }
+    }
+}
