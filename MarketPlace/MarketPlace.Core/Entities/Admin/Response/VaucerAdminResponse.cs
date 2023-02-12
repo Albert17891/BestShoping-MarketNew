ae8709d6c8a6ee0658da9a3f62df401c453919e2
Albert <user@WINDOWS-O7N9M38>
2023-02-12 12:41:25 +0400

    add new Entity

--- MarketPlace/MarketPlace.Core/Entities/Admin/Response/VaucerAdminResponse.cs
+++ MarketPlace/MarketPlace.Core/Entities/Admin/Response/VaucerAdminResponse.cs
@@ -6,4 +6,6 @@ public class VaucerAdminResponse
     public DateTime ExpireTime { get; set; }
     public string UserName { get; set; }
     public string ProductName { get; set; }
+
+    public bool IsUsed { get; set; }
 }
