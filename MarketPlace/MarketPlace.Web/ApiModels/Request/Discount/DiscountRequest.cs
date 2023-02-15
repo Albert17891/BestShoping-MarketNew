1d9aed7dccaaddab389075b0a2d0681bdc7eaf2d
Albert <user@WINDOWS-O7N9M38>
2023-02-15 11:52:59 +0400

    add new ApiModel

---
+++ MarketPlace/MarketPlace.Web/ApiModels/Request/Discount/DiscountRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MarketPlace.Web.ApiModels.Request.Discount;
+
+public class DiscountRequest
+{
+    public int ProductId { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+    public int Percent { get; set; }
+}
