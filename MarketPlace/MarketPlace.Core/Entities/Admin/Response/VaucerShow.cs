f5c7e67c2ba564919136c7928909b73dd3dabe7d
Albert <user@WINDOWS-O7N9M38>
2023-02-19 14:04:05 +0400

    add new model vaucershow

---
+++ MarketPlace/MarketPlace.Core/Entities/Admin/Response/VaucerShow.cs
@@ -0,0 +1,7 @@
+﻿namespace MarketPlace.Core.Entities.Admin.Response;
+public class VaucerShow
+{
+    public string UserName { get; set; }
+    public DateTime ExpireTime { get; set; }
+    public bool IsUsed { get; set; }
+}
