3ec48cb833abbd5fb8dc76b5822fe504f7d28b8f
Albert <user@WINDOWS-O7N9M38>
2023-03-14 16:57:33 +0400

    using  dapper istead ef.core

--- MarketPlace/MarketPlace.Core/Handlers/QueryHandlers/GetMyProductHandler.cs
+++ MarketPlace/MarketPlace.Core/Handlers/QueryHandlers/GetMyProductHandler.cs
@@ -1,25 +1,49 @@
-﻿using MarketPlace.Core.Entities;
+﻿using Dapper;
+using MarketPlace.Core.Entities;
+using MarketPlace.Core.Interfaces.DapperInterfaces;
 using MarketPlace.Core.Interfaces.Repository;
 using MarketPlace.Core.Queries;
 using MediatR;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace MarketPlace.Core.Handlers.QueryHandlers;
 
 public class GetMyProductHandler : IRequestHandler<GetMyProductQuery, IList<Product>>
 {
-    private readonly IUnitOfWork _unitOfWork;
+    private readonly ISqlConnectionFactory _connectionFactory;
 
-    public GetMyProductHandler(IUnitOfWork unitOfWork)
+    public GetMyProductHandler(ISqlConnectionFactory connectionFactory)
     {
-        _unitOfWork = unitOfWork;
+        _connectionFactory = connectionFactory;
     }
+    //private readonly IUnitOfWork _unitOfWork;
 
+    //public GetMyProductHandler(IUnitOfWork unitOfWork)
+    //{
+    //    _unitOfWork = unitOfWork;
+    //}
+
+    //public async Task<IList<Product>> Handle(GetMyProductQuery request, CancellationToken cancellationToken)
+    //{
+    //    var products = await _unitOfWork.Repository<Product>().Table.Where(x => x.OwnerUserId == request.UserId)
+    //                                                          .ToListAsync(cancellationToken);
+
+    //    return products;
+    //}
     public async Task<IList<Product>> Handle(GetMyProductQuery request, CancellationToken cancellationToken)
     {
-        var products = await _unitOfWork.Repository<Product>().Table.Where(x => x.OwnerUserId == request.UserId)
-                                                              .ToListAsync(cancellationToken);
+        await using  var sqlConnection = _connectionFactory.CreateConnection();
 
-        return products;
+        var products = await sqlConnection.QueryAsync<Product>(
+             @"SELECT * 
+                 From Products
+                  Where OwnerUserId=@UserId",
+        new
+        {
+            request.UserId
+        }) ;
+
+        return products.ToList();
     }
 }
